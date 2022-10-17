d3b4f2e338eeb056f13d0f891dd1d12ced3e5ea7
xiaohongchao <zhangyan@2012>
2022-10-17 19:00:19 +0800

    调整xhc-common模块

---
+++ xhc-common/src/test/java/org/example/xhc/common/util/FileUtilsTest.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2022-2025 xiaohongchao.All Rights Reserved.
+ */
+
+package org.example.xhc.common.util;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * TODO
+ *
+ * @author xiaohongchao
+ * @since 1.0.0
+ */
+class FileUtilsTest {
+
+    @Test
+    void readString() {
+    }
+}
