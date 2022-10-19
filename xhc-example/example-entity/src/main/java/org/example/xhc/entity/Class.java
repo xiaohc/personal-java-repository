79a85f748944178cdad03a900ca7bd6388c6e1c8
xiaohongchao <zhangyan@2012>
2022-10-19 15:12:40 +0800

    新增学生实体类

---
+++ xhc-example/example-entity/src/main/java/org/example/xhc/entity/Class.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2022 xiaohongchao.All Rights Reserved.
+ */
+
+package org.example.xhc.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * 班级
+ *
+ * @author xiaohongchao
+ * @since 1.0.0
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Class {
+    /**
+     * 班级号
+     */
+    private String no;
+
+    /**
+     * 班级名称
+     */
+    private String name;
+}
