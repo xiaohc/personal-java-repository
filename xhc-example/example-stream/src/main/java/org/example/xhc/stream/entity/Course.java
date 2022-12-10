802ff05fa580c64b7d663b49edec005f925f4903
xiaohongchao <zhangyan@2012>
2022-12-10 11:47:20 +0800

    调整实体类

---
+++ xhc-example/example-stream/src/main/java/org/example/xhc/stream/entity/Course.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2022 xiaohongchao.All Rights Reserved.
+ */
+
+package org.example.xhc.stream.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * 课程
+ *
+ * @author xiaohongchao
+ * @since 1.0.0
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Course {
+    /**
+     * 课程号
+     */
+    private String no;
+
+    /**
+     * 课程名
+     */
+    private String name;
+}
---
+++ xhc-example/example-stream/src/main/java/org/example/xhc/stream/entity/Student.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2022 xiaohongchao.All Rights Reserved.
+ */
+
+package org.example.xhc.stream.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.example.xhc.entity.type.SexEnum;
+
+import java.time.LocalDateTime;
+
+/**
+ * 学生
+ *
+ * @author xiaohongchao
+ * @since 1.0.0
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Student {
+    /**
+     * 学号
+     */
+    private String no;
+
+    /**
+     * 姓名
+     */
+    private String name;
+
+    /**
+     * 性别
+     */
+    private SexEnum sex;
+
+    /**
+     * 年龄
+     */
+    private Integer age;
+
+    /**
+     * 生日
+     */
+    private LocalDateTime birthday;
+
+    /**
+     * 班级号
+     */
+    private String classNo;
+}
