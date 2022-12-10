0fde02c3ea8a4ddccec380d608c831d79e1cf2e9
xiaohongchao <zhangyan@2012>
2022-12-10 17:13:11 +0800

    调整mybatis自动生成代码插件和配置

--- xhc-example/example-mybatis-generator/src/main/java/org/mybatis/generator/plugins/LombokPlugin.java
+++ xhc-example/example-mybatis-generator/src/main/java/org/mybatis/generator/plugins/LombokPlugin.java
@@ -7,12 +7,9 @@ package org.mybatis.generator.plugins;
 import org.mybatis.generator.api.IntrospectedColumn;
 import org.mybatis.generator.api.IntrospectedTable;
 import org.mybatis.generator.api.PluginAdapter;
-import org.mybatis.generator.api.dom.java.Interface;
-import org.mybatis.generator.api.dom.java.Method;
-import org.mybatis.generator.api.dom.java.TopLevelClass;
+import org.mybatis.generator.api.dom.java.*;
+import org.mybatis.generator.internal.util.StringUtility;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.List;
 
 /**
@@ -23,8 +20,6 @@ import java.util.List;
  * @since 1.0.0
  */
 public class LombokPlugin extends PluginAdapter {
-    public LombokPlugin() {
-    }
 
     @Override
     public boolean validate(List<String> list) {
@@ -33,37 +28,79 @@ public class LombokPlugin extends PluginAdapter {
 
     @Override
     public boolean modelBaseRecordClassGenerated(TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
+        if (isHasLombok()) {
+            addLombokContent(topLevelClass);
+        }
+
+        addFileHeader(topLevelClass, introspectedTable);
+
+        return true;
+    }
+
+    private void addFileHeader(TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
+        topLevelClass.addJavaDocLine("/**");
+
+        addJavaDocLineOnElement(topLevelClass, introspectedTable.getRemarks());
+
+        topLevelClass.addJavaDocLine(" * " + introspectedTable.getFullyQualifiedTable());
+        topLevelClass.addJavaDocLine(" * @author xiaohongchao");
+        topLevelClass.addJavaDocLine(" * @since 1.0.0");
+
+        topLevelClass.addJavaDocLine(" */");
+    }
+
+    private void addLombokContent(TopLevelClass topLevelClass) {
+        // 添加 lombok 依赖
         topLevelClass.addImportedType("lombok.Data");
         topLevelClass.addImportedType("lombok.Builder");
         topLevelClass.addImportedType("lombok.NoArgsConstructor");
         topLevelClass.addImportedType("lombok.AllArgsConstructor");
+
+        // 添加 lombok 注解
         topLevelClass.addAnnotation("@Data");
-        topLevelClass.addJavaDocLine("/**");
-        topLevelClass.addJavaDocLine("* Created by Mybatis Generator on " + this.date2Str(new Date()));
-        topLevelClass.addJavaDocLine("*/");
-        return true;
+        topLevelClass.addAnnotation("@Builder");
+        topLevelClass.addAnnotation("@NoArgsConstructor");
+        topLevelClass.addAnnotation("@AllArgsConstructor");
     }
 
-    public boolean clientGenerated(Interface interfaze, TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
-        interfaze.addJavaDocLine("/**");
-        interfaze.addJavaDocLine("* Created by Mybatis Generator on " + this.date2Str(new Date()));
-        interfaze.addJavaDocLine("*/");
+    @Override
+    public boolean modelFieldGenerated(Field field, TopLevelClass topLevelClass, IntrospectedColumn introspectedColumn, IntrospectedTable introspectedTable, ModelClassType modelClassType) {
+        field.addJavaDocLine("/**");
+        addJavaDocLineOnElement(field, introspectedColumn.getRemarks());
+        field.addJavaDocLine(" */");
         return true;
     }
 
     @Override
-    public boolean modelSetterMethodGenerated(Method method, TopLevelClass topLevelClass, IntrospectedColumn introspectedColumn, IntrospectedTable introspectedTable, ModelClassType modelClassType) {
-        return false;
+    public boolean clientGenerated(Interface interfaze, IntrospectedTable introspectedTable) {
+        // 添加Mapper的import
+        interfaze.addImportedType(new FullyQualifiedJavaType("org.apache.ibatis.annotations.Mapper"));
+        // 添加Mapper的注解
+        interfaze.addAnnotation("@Mapper");
+        return true;
     }
 
     @Override
-    public boolean modelGetterMethodGenerated(Method method, TopLevelClass topLevelClass, IntrospectedColumn introspectedColumn, IntrospectedTable introspectedTable, ModelClassType modelClassType) {
-        return false;
+    public boolean modelSetterMethodGenerated(Method method, TopLevelClass topLevelClass, IntrospectedColumn introspectedColumn, IntrospectedTable introspectedTable, ModelClassType modelClassType) { // 不生成getter
+        return !isHasLombok();
     }
 
-    private String date2Str(Date date) {
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
-        return sdf.format(date);
+    @Override
+    public boolean modelGetterMethodGenerated(Method method, TopLevelClass topLevelClass, IntrospectedColumn introspectedColumn, IntrospectedTable introspectedTable, ModelClassType modelClassType) { // 不生成setter
+        return !isHasLombok();
+    }
+
+    private boolean isHasLombok() {
+        return Boolean.parseBoolean(properties.getProperty("hasLombok", "false"));
+    }
+
+    private void addJavaDocLineOnElement(JavaElement element, String remarks) {
+        if (StringUtility.stringHasValue(remarks)) {
+            String[] remarkLines = remarks.split(System.getProperty("line.separator"));
+            for (String remarkLine : remarkLines) {
+                element.addJavaDocLine(" * " + remarkLine);
+            }
+        }
     }
 }
 
